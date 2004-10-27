a63c6af4d1405dc47d10e3f7aedaea3a8a7682e8
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2004-10-27 10:42:18 +0000

    Added support for locale-specific stock lists

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/internal/views/StockList.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/internal/views/StockList.java
@@ -10,27 +10,17 @@
  *******************************************************************************/
 package net.sourceforge.eclipsetrader.ui.internal.views;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Comparator;
+import java.util.Locale;
 import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import net.sourceforge.eclipsetrader.BasicData;
 import net.sourceforge.eclipsetrader.IBasicData;
@@ -38,100 +28,20 @@ import net.sourceforge.eclipsetrader.IBasicData;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
- * @author Marco
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class StockList
 {
-  private static String FILE_NAME = "stocklist.xml"; //$NON-NLS-1$
+  private static String FILE_NAME = "stocklist"; //$NON-NLS-1$
+  private static String FILE_EXT = ".xml"; //$NON-NLS-1$
   private IBasicData[] data;
   
   public StockList()
   {
-/*    try {
-      loadFromSite();
-    } catch(Exception e) {};*/
     load();
-  }
-  
-  private void loadFromSite() throws Exception
-  {
-    int s, e;
-    String line, ticker = "", isin = "", description = "";
-    Vector v = new Vector();
-    
-    for (char letter = 'A'; letter <= 'Z'; letter++)
-    {
-      URL url = new URL("http://www.borsaitalia.it/servlet/HomeController?param_iniziale=" + letter + "&target=SearchQuotes");
-      System.out.println(url);
-      HttpURLConnection.setFollowRedirects(true);
-      HttpURLConnection con = (HttpURLConnection)url.openConnection();
-      con.setAllowUserInteraction(true);
-      con.setRequestMethod("GET");
-      con.connect();
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      while ((line = in.readLine()) != null) 
-      {
-        if (line.indexOf("target=PersonalSiteStock") != -1)
-        {
-          if ((s = line.indexOf("id_int=AF")) != -1)
-          {
-            s += 9;
-            if ((e = line.indexOf("&", s)) > s)
-            {
-              ticker = line.substring(s, e);
-              
-              s = line.indexOf("isin=") + 5;
-              e = line.indexOf("'", s);
-              isin = line.substring(s, e);
-            }
-          }
-        }
-        else if (line.indexOf("Controller?target=Display") != -1)
-        {
-          if ((s = line.indexOf("isin=")) != -1)
-          {
-            s += 5;
-            if ((e = line.indexOf("\"", s)) > s)
-            {
-              if (line.substring(s, e).equalsIgnoreCase(isin) == true)
-              {
-                s = line.indexOf(">", s) + 1;
-                e = line.indexOf("<", s);
-                description = line.substring(s, e);
-
-                IBasicData data = new BasicData();
-                data.setSymbol(isin);
-                data.setTicker(ticker);
-                data.setDescription(description);
-                v.addElement(data);
-                
-                System.out.println(description + "," + ticker + "," + isin);
-              }
-            }
-          }
-        }
-      }
-    }
-
-    // Sort the items
-    java.util.Collections.sort(v, new Comparator() {
-      public int compare(Object o1, Object o2) {
-        return ((IBasicData)o1).getDescription().compareTo(((IBasicData)o2).getDescription());
-      }
-    });
-    
-    data = new IBasicData[v.size()];
-    v.toArray(data);
-    
-    store();
   }
 
   private void load()
@@ -139,19 +49,36 @@ public class StockList
     Vector v = new Vector();
     InputStream is = null;
     
-    // Attempt to read the map file from the workspace location
-    File f = new File(Platform.getLocation().toFile(), FILE_NAME);
+    // Attempt to read a locale-specific file from the workspace
+    File f = new File(Platform.getLocation().toFile(), FILE_NAME + Locale.getDefault().getCountry().toLowerCase() + FILE_EXT);
+    System.out.println(f);
     if (f.exists() == true)
     {
       try {
         is = new FileInputStream(f);
       } catch (FileNotFoundException e) {}
     }
-    // Attempt to read the default map file from the plugin's install location
+    // Attempt to read a locale-specific file from the plugin's install location
     if (is == null)
     {
       try {
-        is = ViewsPlugin.getDefault().openStream(new Path(FILE_NAME));
+        is = ViewsPlugin.getDefault().openStream(new Path(FILE_NAME + Locale.getDefault().getCountry().toLowerCase() + FILE_EXT));
+      } catch (IOException e) {}
+    }
+    
+    // Attempt to read the file from the workspace location
+    f = new File(Platform.getLocation().toFile(), FILE_NAME + FILE_EXT);
+    if (f.exists() == true)
+    {
+      try {
+        is = new FileInputStream(f);
+      } catch (FileNotFoundException e) {}
+    }
+    // Attempt to read the file from the plugin's install location
+    if (is == null)
+    {
+      try {
+        is = ViewsPlugin.getDefault().openStream(new Path(FILE_NAME + FILE_EXT));
       } catch (IOException e) {}
     }
     
@@ -204,41 +131,6 @@ public class StockList
     
     data = new IBasicData[v.size()];
     v.toArray(data);
-  }
-
-  private void store()
-  {
-    try {
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder builder = factory.newDocumentBuilder();
-      Document document = builder.getDOMImplementation().createDocument("", "stocklist", null);
-
-      for (int i = 0; i < data.length; i++)
-      {
-        Element element = document.createElement("data");
-        element.setAttribute("symbol", data[i].getSymbol());
-        element.setAttribute("ticker", data[i].getTicker());
-        document.getDocumentElement().appendChild(element);
-
-        Node node = document.createElement("description");
-        element.appendChild(node);
-        node.appendChild(document.createTextNode(data[i].getDescription()));
-        node = document.createElement("minimum_quantity");
-        element.appendChild(node);
-        node.appendChild(document.createTextNode("1"));
-      }
-
-      Transformer transformer = TransformerFactory.newInstance().newTransformer();
-      transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
-      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-      transformer.setOutputProperty("{http\u003a//xml.apache.org/xslt}indent-amount", "4");
-      DOMSource source = new DOMSource(document);
-      BufferedWriter out = new BufferedWriter(new FileWriter(new File(Platform.getLocation().toFile(), "stocklist1.xml")));
-      StreamResult result = new StreamResult(out);
-      transformer.transform(source, result);
-      out.flush();
-      out.close();
-    } catch (Exception ex) { ex.printStackTrace(); };
   }
   
   public IBasicData[] getData()
