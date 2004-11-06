db09022c3ca2f32bd1f8049c9731e7e179639d2f
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2004-11-06 08:45:02 +0000

    Initial commit

---
+++ net.sourceforge.eclipsetrader/src/net/sourceforge/eclipsetrader/RealtimeChartDataProvider.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ *******************************************************************************/
+package net.sourceforge.eclipsetrader;
+
+
+/**
+ * Default implementation of the IRealtimeChartProvider interface.
+ * <p>Plugin developers may extend this class instead of implementing the interface and
+ * override the startStreaming and stopStreaming methods only.</p>
+ * <p></p>
+ * 
+ * @author Marco Maccaferri
+ */
+public class RealtimeChartDataProvider extends DataProvider implements IRealtimeChartProvider
+{
+  private DataCollector dataCollector = new DataCollector(this);
+
+  /**
+   * Notify the listeners of a data update event, and collects the data for
+   * realtime chart generation.
+   * <p></p>
+   */
+  public void fireDataUpdated()
+  {
+    super.fireDataUpdated();
+    dataCollector.dataUpdated(this);
+  }
+  
+  /**
+   * Notify the listeners of a data update event, and collects the data for
+   * realtime chart generation.
+   * <p></p>
+   */
+  public void fireDataUpdated(IBasicData data)
+  {
+    super.fireDataUpdated(data);
+    dataCollector.dataUpdated(this, data);
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.IRealtimeChartProvider#addRealtimeChartListener(net.sourceforge.eclipsetrader.IBasicData, net.sourceforge.eclipsetrader.IRealtimeChartListener)
+   */
+  public void addRealtimeChartListener(IBasicData data, IRealtimeChartListener listener)
+  {
+    dataCollector.addRealtimeChartListener(data, listener);
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.IRealtimeChartProvider#backfill(net.sourceforge.eclipsetrader.IBasicData)
+   */
+  public void backfill(IBasicData data)
+  {
+  }
+  
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.IRealtimeChartProvider#getHistoryData(net.sourceforge.eclipsetrader.IBasicData)
+   */
+  public IChartData[] getHistoryData(IBasicData data)
+  {
+    return dataCollector.getData(data.getSymbol());
+  }
+  
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.IRealtimeChartProvider#removeRealtimeChartListener(net.sourceforge.eclipsetrader.IBasicData, net.sourceforge.eclipsetrader.IRealtimeChartListener)
+   */
+  public void removeRealtimeChartListener(IBasicData data, IRealtimeChartListener listener)
+  {
+    dataCollector.removeRealtimeChartListener(data, listener);
+  }
+}
