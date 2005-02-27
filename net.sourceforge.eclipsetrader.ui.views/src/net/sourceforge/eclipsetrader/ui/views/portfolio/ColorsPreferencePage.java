05c8534ac2848da6da5c013cb3ca56c4c26d9cae
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-02-27 10:48:42 +0000

    Removed deprecated methods calls

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/portfolio/ColorsPreferencePage.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/portfolio/ColorsPreferencePage.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2004 Marco Maccaferri and others.
+ * Copyright (c) 2004-2005 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
@@ -68,7 +68,6 @@ public class ColorsPreferencePage extends PreferencePage implements IWorkbenchPr
     for (int i = 0; i < _v.size(); i++)
     {
       editor[i] = (FieldEditor)_v.elementAt(i);
-      editor[i].setPreferencePage(this);
       editor[i].setPreferenceStore(getPreferenceStore());
       editor[i].load();
     }
