c95e66c7d76ed943342155311a7bcea91af33bf1
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-02-27 17:04:08 +0000

    Changes to track chart status changes.

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/RealtimeChartActions.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/RealtimeChartActions.java
@@ -14,45 +14,50 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 
 /**
  */
 public class RealtimeChartActions implements IViewActionDelegate
 {
-  private static RealtimeChartView view;
 
   /* (non-Javadoc)
    * @see org.eclipse.ui.IViewActionDelegate#init(org.eclipse.ui.IViewPart)
    */
   public void init(IViewPart viewPart)
   {
-    if (viewPart instanceof RealtimeChartView)
-      view = (RealtimeChartView)viewPart;
   }
-
+  
   /* (non-Javadoc)
    * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
    */
   public void run(IAction action)
   {
-    if (action.getId().equalsIgnoreCase("chart.add") == true)
+    IWorkbenchPage pg = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+    if (pg.getActivePart() instanceof RealtimeChartView)
     {
-      OscillatorDialog dlg = new OscillatorDialog();
-      if (dlg.open() == OscillatorDialog.OK)
-        view.addOscillator(dlg.getId());
+      RealtimeChartView view = (RealtimeChartView)pg.getActivePart();
+
+      if (action.getId().equalsIgnoreCase("chart.add") == true)
+      {
+        OscillatorDialog dlg = new OscillatorDialog();
+        if (dlg.open() == OscillatorDialog.OK)
+          view.addOscillator(dlg.getId());
+      }
+      else if (action.getId().equalsIgnoreCase("chart.edit") == true)
+        view.editOscillator();
+      else if (action.getId().equalsIgnoreCase("chart.remove") == true)
+        view.removeOscillator();
+      else if (action.getId().equalsIgnoreCase("chart.line") == true)
+        view.setChartType(PriceChart.LINE);
+      else if (action.getId().equalsIgnoreCase("chart.candle") == true)
+        view.setChartType(PriceChart.CANDLE);
+      else if (action.getId().equalsIgnoreCase("chart.bar") == true)
+        view.setChartType(PriceChart.BAR);
+      else if (action.getId().equalsIgnoreCase("chart.refresh") == true)
+        view.refreshChart();
     }
-    else if (action.getId().equalsIgnoreCase("chart.edit") == true)
-      view.editOscillator();
-    else if (action.getId().equalsIgnoreCase("chart.remove") == true)
-      view.removeOscillator();
-    else if (action.getId().equalsIgnoreCase("chart.line") == true)
-      view.setChartType(PriceChart.LINE);
-    else if (action.getId().equalsIgnoreCase("chart.candle") == true)
-      view.setChartType(PriceChart.CANDLE);
-    else if (action.getId().equalsIgnoreCase("chart.bar") == true)
-      view.setChartType(PriceChart.BAR);
-    else if (action.getId().equalsIgnoreCase("chart.refresh") == true)
-      view.refreshChart();
   }
 
   /* (non-Javadoc)
@@ -60,5 +65,17 @@ public class RealtimeChartActions implements IViewActionDelegate
    */
   public void selectionChanged(IAction action, ISelection selection)
   {
+    IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+    if (page != null && page.getActivePart() instanceof RealtimeChartView)
+    {
+      RealtimeChartView view = (RealtimeChartView)page.getActivePart();
+
+      if (action.getId().equalsIgnoreCase("chart.line") == true)
+        action.setChecked(view.getChartType() == PriceChart.LINE);
+      else if (action.getId().equalsIgnoreCase("chart.candle") == true)
+        action.setChecked(view.getChartType() == PriceChart.CANDLE);
+      else if (action.getId().equalsIgnoreCase("chart.bar") == true)
+        action.setChecked(view.getChartType() == PriceChart.BAR);
+    }
   }
 }
