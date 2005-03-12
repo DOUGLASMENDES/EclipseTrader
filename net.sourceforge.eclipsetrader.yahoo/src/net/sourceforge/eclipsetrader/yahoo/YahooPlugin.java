de4e082b94b8a6a257804432a081b2e11d621a79
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-03-12 10:15:34 +0000

    Added setting of locale-specific default values.

--- net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/YahooPlugin.java
+++ net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/YahooPlugin.java
@@ -1,15 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2004 Marco Maccaferri and others.
+ * Copyright (c) 2004-2005 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
  *******************************************************************************/
 package net.sourceforge.eclipsetrader.yahoo;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
@@ -48,31 +49,42 @@ public class YahooPlugin extends AbstractUIPlugin implements IPropertyChangeList
   {
 		super.start(context);
 
-    // Sets the default preferences
+    // Sets the default preferences based on the locale settings
     IPreferenceStore ps = getPreferenceStore();
     ps.setDefault("yahoo.refresh", "15");
-    ps.setDefault("yahoo.url", "http://finance.yahoo.com/d/quotes.csv");
+    String locale = Locale.getDefault().getCountry().toLowerCase();
+    if (locale.length() == 0 || locale.equalsIgnoreCase("us"))
+      ps.setDefault("yahoo.url", "http://finance.yahoo.com/d/quotes.csv");
+    else
+      ps.setDefault("yahoo.url", "http://" + locale + ".finance.yahoo.com/d/quotes.csv");
     ps.setDefault("yahoo.charts.url", "http://table.finance.yahoo.com/table.csv");
-    ps.setDefault("yahoo.mapping", false);
-    ps.setDefault("yahoo.suffix", "");
-    ps.setDefault("net.sourceforge.eclipsetrader.yahoo.newsSource", "yahoo.news.us");
+    if (locale.equalsIgnoreCase("it"))
+    {
+      ps.setDefault("yahoo.field", 1);
+      ps.setDefault("yahoo.mapping", true);
+      ps.setDefault("yahoo.charts.field", 1);
+      ps.setDefault("yahoo.charts.mapping", true);
+      ps.setDefault("yahoo.suffix", ".MI");
+      ps.setDefault("net.sourceforge.eclipsetrader.yahoo.newsSource", "yahoo.news.it");
+    }
+    else if (locale.equalsIgnoreCase("de"))
+    {
+      ps.setDefault("yahoo.field", 0);
+      ps.setDefault("yahoo.mapping", false);
+      ps.setDefault("yahoo.charts.field", 1);
+      ps.setDefault("yahoo.charts.mapping", true);
+      ps.setDefault("yahoo.suffix", ".DE");
+    }
+    else if (locale.equalsIgnoreCase("no"))
+    {
+      ps.setDefault("yahoo.mapping", true);
+      ps.setDefault("yahoo.charts.mapping", true);
+      ps.setDefault("yahoo.suffix", ".OL");
+    }
+    else
+      ps.setDefault("net.sourceforge.eclipsetrader.yahoo.newsSource", "yahoo.news.us");
     ps.setDefault("NEW_CHART_YEARS", "1");
     ps.addPropertyChangeListener(this);
-    
-//    SymbolMapper.setDoMapping(ps.getBoolean("yahoo.mapping"));
-//    SymbolMapper.setDefaultSuffix(ps.getString("yahoo.suffix"));
-    
-/*    PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-      public void run() {
-        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-        MessageBox msg = new MessageBox(shell, SWT.ICON_INFORMATION|SWT.YES|SWT.NO);
-        msg.setMessage("Do you want to start the data streamer ?");
-        if (msg.open() == SWT.YES)
-        {
-          TraderPlugin.getDataProvider().startStreaming();
-        }
-      }
-    });*/
 	}
 
 	/**
