0cebdee5e2a44eaf4f4b05677d9e6b3eb9c3e93c
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-05-12 13:38:56 +0000

    Changed parameters layout

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/ChartParametersDialog.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/ChartParametersDialog.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2004 Marco Maccaferri and others.
+ * Copyright (c) 2004-2005 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
@@ -35,13 +35,14 @@ import org.eclipse.ui.PlatformUI;
 public class ChartParametersDialog extends TitleAreaDialog
 {
   public final static int SELECTED_ZONE = 1;
-  public final static int NEW_ZONE = 2;
+  public final static int BELOW_SELECTED_ZONE = 2;
+  public final static int ABOVE_SELECTED_ZONE = 3;
   public static final int NEW_CHART = 1;
   public static final int EDIT_CHART = 2;
   private int type = NEW_CHART;
   protected int position = SELECTED_ZONE;
+  private Composite composite;
 //  private Text chartName;
-  private Group paramGroup;
   private Button selectedZone;
   private Button newZone;
   private ColorSelector colorSelector;
@@ -66,55 +67,28 @@ public class ChartParametersDialog extends TitleAreaDialog
    */
   protected Control createDialogArea(Composite parent)
   {
-    Composite composite = new Composite(parent, SWT.NONE);
+    composite = new Composite(parent, SWT.NONE);
     composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-    composite.setLayout(new GridLayout(1, false));
+    composite.setLayout(new GridLayout(2, false));
     
-    paramGroup = new Group(composite, SWT.NONE);
-    paramGroup.setText(Messages.getString("ChartParametersDialog.description")); //$NON-NLS-1$
-    paramGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
-    paramGroup.setLayout(new GridLayout(2, false));
-    
-    Label label = new Label(paramGroup, SWT.NONE);
+    Label label = new Label(composite, SWT.NONE);
     label.setText(Messages.getString("ChartParametersDialog.name")); //$NON-NLS-1$
-    label.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL|GridData.HORIZONTAL_ALIGN_FILL));
-    Text text = new Text(paramGroup, SWT.BORDER);
+    label.setLayoutData(new GridData(125, SWT.DEFAULT));
+    Text text = new Text(composite, SWT.BORDER);
     text.setData("name"); //$NON-NLS-1$
     text.setText(chartConfigurer.getName());
-    GridData gridData = new GridData();
-    gridData.widthHint = 175;
-    text.setLayoutData(gridData);
+    text.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL|GridData.HORIZONTAL_ALIGN_FILL));
+    
+    label = new Label(composite, SWT.NONE);
+    label.setText(Messages.getString("ChartParametersDialog.color")); //$NON-NLS-1$
+    colorSelector = new ColorSelector(composite);
+    colorSelector.getButton().setData("color"); //$NON-NLS-1$
+    colorSelector.setColorValue(chartConfigurer.getColorParameter("color")); //$NON-NLS-1$
 
     // Adds the parameters specific to the given chart
     if (chartConfigurer != null)
-      chartConfigurer.createContents(paramGroup);
+      chartConfigurer.createContents(composite);
     
-    label = new Label(paramGroup, SWT.NONE);
-    label.setText(Messages.getString("ChartParametersDialog.color")); //$NON-NLS-1$
-    label.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL|GridData.HORIZONTAL_ALIGN_FILL));
-    colorSelector = new ColorSelector(paramGroup);
-    colorSelector.getButton().setData("color"); //$NON-NLS-1$
-    colorSelector.setColorValue(chartConfigurer.getColorParameter("color")); //$NON-NLS-1$
-    
-/*    group = new Group(composite, SWT.NONE);
-    group.setText("Parametri grafico");
-    group.setLayoutData(new GridData(GridData.FILL_BOTH));
-    group.setLayout(new GridLayout(2, false));
-    
-    Combo combo1 = new Combo(group, SWT.READ_ONLY|SWT.BORDER);
-    combo1.add("CAPITALIA");
-    combo1.add("Media Mobile (30)");
-    combo1.select(0);
-    combo1.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL|GridData.HORIZONTAL_ALIGN_FILL));
-    Combo combo2 = new Combo(group, SWT.READ_ONLY|SWT.BORDER);
-    combo2.add("Chiusura");
-    combo2.add("Apertura");
-    combo2.add("Massimo");
-    combo2.add("Minimo");
-    combo2.select(0);
-    gridData = new GridData();
-    combo2.setLayoutData(gridData);*/
-
     if (type == NEW_CHART)
     {
       Group group = new Group(composite, SWT.NONE);
@@ -172,13 +146,13 @@ public class ChartParametersDialog extends TitleAreaDialog
       if (selectedZone.getSelection() == true)
         position = SELECTED_ZONE;
       else if (newZone.getSelection() == true)
-        position = NEW_ZONE;
+        position = BELOW_SELECTED_ZONE;
     }
 
     RGB rgb = colorSelector.getColorValue(); 
     chartConfigurer.setParameter("color", String.valueOf(rgb.red) + "," + String.valueOf(rgb.green) + "," + String.valueOf(rgb.blue)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     
-    Control[] c = paramGroup.getChildren();
+    Control[] c = composite.getChildren();
     for (int i = 0; i < c.length; i++)
     {
       if (c[i].getData() == null || !(c[i].getData() instanceof String))
