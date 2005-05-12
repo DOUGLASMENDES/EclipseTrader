bf493407740938358e1f4c9edd93b1d9a6f7f8a4
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-05-12 13:03:46 +0000

    Changed calculation method and parameters

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/AccumulationDistributionChart.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/AccumulationDistributionChart.java
@@ -1,33 +1,40 @@
 /*******************************************************************************
- * Copyright (c) 2004 Marco Maccaferri and others.
+ * Copyright (c) 2004-2005 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
+ *     Stefan S. Stratigakos - Original Qtstalker code
  *******************************************************************************/
 package net.sourceforge.eclipsetrader.ui.views.charts;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import net.sourceforge.eclipsetrader.ui.internal.views.Messages;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 
 /**
- * Moving Average chart.
+ * Accumulation Distribution.
  * <p></p>
- * 
- * @author Marco Maccaferri
  */
 public class AccumulationDistributionChart extends ChartPlotter implements IChartConfigurer
 {
+  public static final int STANDARD = 0;
+  public static final int WILLIAM = 1;
   public static String PLUGIN_ID = "net.sourceforge.eclipsetrader.charts.accumulationDistribution"; //$NON-NLS-1$
   private Color gridColor = new Color(null, 192, 192, 192);
+  private int type = STANDARD;
   
   public AccumulationDistributionChart()
   {
@@ -55,8 +62,6 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
    */
   public void paintChart(GC gc, int width, int height)
   {
-    double min = 0, max = 0;
-    
     super.paintChart(gc, width, height);
     if (chartData != null)
     {
@@ -70,20 +75,75 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
       // Calculate the values
       if (chartData.length >= 1)
       {
-        double previous = 0;
-        double[] value = new double[chartData.length];
-        for (int i = 0; i < value.length; i++)
+        double min = 0, max = 0;
+        List list = new ArrayList();
+
+        if (type == STANDARD)
         {
-          double v1 = chartData[i].getClosePrice() - chartData[i].getMinPrice();
-          double v2 = chartData[i].getMaxPrice() - chartData[i].getClosePrice();
-          double v3 = chartData[i].getMaxPrice() - chartData[i].getMinPrice();
-          if (v3 != 0)
-            value[i] = (((v1 - v2) / v3) * chartData[i].getVolume()) + previous;
-          if (min == 0 || value[i] < min)
-            min = value[i];
-          if (value[i] > max)
-            max = value[i];
-          previous = value[i];
+          int loop;
+          double accum = 0;
+          for (loop = 0; loop < chartData.length; loop++)
+          {
+            int volume = chartData[loop].getVolume();
+            if (volume > 0)
+            {
+              double high = chartData[loop].getMaxPrice();
+              double low = chartData[loop].getMinPrice();
+
+              double t = high - low;
+
+              if (t != 0)
+              {
+                double close = chartData[loop].getClosePrice();
+                double t2 = (close - low) - (high - close);
+                accum = accum + ((t2 / t) * volume);
+              }
+            }
+
+            list.add(new Double(accum));
+
+            if (min == 0 || accum < min)
+              min = accum;
+            if (accum > max)
+              max = accum;
+          }
+        }
+        else
+        {
+          int loop;
+          double accum = 0;
+          for (loop = 1; loop < chartData.length; loop++)
+          {
+            double high = chartData[loop].getMaxPrice();
+            double low = chartData[loop].getMinPrice();
+            double close = chartData[loop].getClosePrice();
+            double yclose = chartData[loop - 1].getClosePrice();
+
+            double h = high;
+            if (yclose > h)
+              h = yclose;
+
+            double l = low;
+            if (yclose < l)
+              l = yclose;
+
+            if (close > yclose)
+              accum = accum + (close - l);
+            else
+            {
+              if (yclose == close)
+                ;
+              else
+                accum = accum - (h - close);
+            }
+
+            list.add(new Double(accum));
+
+            if (min == 0 || accum < min)
+              min = accum;
+            if (accum > max)
+              max = accum;
+          }
         }
 
         double margin = (max - min) / 100 * 2; 
@@ -91,7 +151,7 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
         min -= margin;
         setMinMax(min, max);
 
-        this.drawLine(value, gc, height);
+        drawLine(list, gc, height);
       }
     }
   }
@@ -108,6 +168,8 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
    */
   public void setParameter(String name, String value)
   {
+    if (name.equalsIgnoreCase("type") == true) //$NON-NLS-1$
+      type = Integer.parseInt(value);
     super.setParameter(name, value);
   }
 
@@ -117,6 +179,14 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
    */
   public Control createContents(Composite parent)
   {
+    Label label = new Label(parent, SWT.NONE);
+    label.setText(Messages.getString("AccumulationDistributionChart.type")); //$NON-NLS-1$
+    Combo combo = new Combo(parent, SWT.READ_ONLY);
+    combo.setData("type"); //$NON-NLS-1$
+    combo.add(Messages.getString("AccumulationDistributionChart.standard")); //$NON-NLS-1$
+    combo.add(Messages.getString("AccumulationDistributionChart.williams")); //$NON-NLS-1$
+    combo.setText(combo.getItem(type));
+
     return parent;
   }
 }
