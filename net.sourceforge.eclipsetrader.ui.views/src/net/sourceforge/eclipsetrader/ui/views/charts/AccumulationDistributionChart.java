f283b728b3a2a50b92f5a0da3bdb06e42853ae3a
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-05-14 10:30:32 +0000

    Optimized calculations and drawing

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/AccumulationDistributionChart.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/AccumulationDistributionChart.java
@@ -14,6 +14,7 @@ package net.sourceforge.eclipsetrader.ui.views.charts;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.eclipsetrader.IChartData;
 import net.sourceforge.eclipsetrader.ui.internal.views.Messages;
 
 import org.eclipse.swt.SWT;
@@ -35,6 +36,7 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
   public static String PLUGIN_ID = "net.sourceforge.eclipsetrader.charts.accumulationDistribution"; //$NON-NLS-1$
   private Color gridColor = new Color(null, 192, 192, 192);
   private int type = STANDARD;
+  private List list = new ArrayList();
   
   public AccumulationDistributionChart()
   {
@@ -56,104 +58,86 @@ public class AccumulationDistributionChart extends ChartPlotter implements IChar
   {
     return getName();
   }
-  
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#setData(net.sourceforge.eclipsetrader.IChartData[])
+   */
+  public void setData(IChartData[] data)
+  {
+    super.setData(data);
+    list = new ArrayList();
+    
+    if (data != null && data.length != 0)
+    {
+      if (type == STANDARD)
+      {
+        int loop;
+        double accum = 0;
+        for (loop = 0; loop < chartData.length; loop++)
+        {
+          int volume = chartData[loop].getVolume();
+          if (volume > 0)
+          {
+            double high = chartData[loop].getMaxPrice();
+            double low = chartData[loop].getMinPrice();
+
+            double t = high - low;
+
+            if (t != 0)
+            {
+              double close = chartData[loop].getClosePrice();
+              double t2 = (close - low) - (high - close);
+              accum = accum + ((t2 / t) * volume);
+            }
+          }
+
+          list.add(new Double(accum));
+        }
+      }
+      else
+      {
+        int loop;
+        double accum = 0;
+        for (loop = 1; loop < chartData.length; loop++)
+        {
+          double high = chartData[loop].getMaxPrice();
+          double low = chartData[loop].getMinPrice();
+          double close = chartData[loop].getClosePrice();
+          double yclose = chartData[loop - 1].getClosePrice();
+
+          double h = high;
+          if (yclose > h)
+            h = yclose;
+
+          double l = low;
+          if (yclose < l)
+            l = yclose;
+
+          if (close > yclose)
+            accum = accum + (close - l);
+          else
+          {
+            if (yclose == close)
+              ;
+            else
+              accum = accum - (h - close);
+          }
+
+          list.add(new Double(accum));
+        }
+      }
+    }
+    
+    setMinMax(list);
+  }
+
   /* (non-Javadoc)
    * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#paintChart(GC gc, int width, int height)
    */
   public void paintChart(GC gc, int width, int height)
   {
     super.paintChart(gc, width, height);
-    if (chartData != null)
-    {
-      gc.setForeground(gridColor);
-      gc.setLineStyle(SWT.LINE_DOT);
-      gc.drawLine(0, height / 2, width, height / 2);
-
-      gc.setLineStyle(SWT.LINE_SOLID);
-      gc.setForeground(getColor());
-
-      // Calculate the values
-      if (chartData.length >= 1)
-      {
-        double min = 0, max = 0;
-        List list = new ArrayList();
-
-        if (type == STANDARD)
-        {
-          int loop;
-          double accum = 0;
-          for (loop = 0; loop < chartData.length; loop++)
-          {
-            int volume = chartData[loop].getVolume();
-            if (volume > 0)
-            {
-              double high = chartData[loop].getMaxPrice();
-              double low = chartData[loop].getMinPrice();
-
-              double t = high - low;
-
-              if (t != 0)
-              {
-                double close = chartData[loop].getClosePrice();
-                double t2 = (close - low) - (high - close);
-                accum = accum + ((t2 / t) * volume);
-              }
-            }
-
-            list.add(new Double(accum));
-
-            if (min == 0 || accum < min)
-              min = accum;
-            if (accum > max)
-              max = accum;
-          }
-        }
-        else
-        {
-          int loop;
-          double accum = 0;
-          for (loop = 1; loop < chartData.length; loop++)
-          {
-            double high = chartData[loop].getMaxPrice();
-            double low = chartData[loop].getMinPrice();
-            double close = chartData[loop].getClosePrice();
-            double yclose = chartData[loop - 1].getClosePrice();
-
-            double h = high;
-            if (yclose > h)
-              h = yclose;
-
-            double l = low;
-            if (yclose < l)
-              l = yclose;
-
-            if (close > yclose)
-              accum = accum + (close - l);
-            else
-            {
-              if (yclose == close)
-                ;
-              else
-                accum = accum - (h - close);
-            }
-
-            list.add(new Double(accum));
-
-            if (min == 0 || accum < min)
-              min = accum;
-            if (accum > max)
-              max = accum;
-          }
-        }
-
-        double margin = (max - min) / 100 * 2; 
-        max += margin;
-        min -= margin;
-        setMinMax(min, max);
-
-        drawLine(list, gc, height);
-      }
-    }
+    drawLine(list, gc, height);
   }
 
   /* (non-Javadoc)
