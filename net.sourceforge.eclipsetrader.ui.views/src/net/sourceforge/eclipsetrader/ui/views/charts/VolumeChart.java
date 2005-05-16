316b3855a7c95b052a03b213df65c09a39c47957
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-05-16 10:16:29 +0000

    Updates to draw the grid lines and scale values

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/VolumeChart.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/VolumeChart.java
@@ -1,30 +1,41 @@
 /*******************************************************************************
- * Copyright (c) 2004 Marco Maccaferri and others.
+ * Copyright (c) 2004-2005 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
  *******************************************************************************/
 package net.sourceforge.eclipsetrader.ui.views.charts;
 
+import java.text.NumberFormat;
+
 import net.sourceforge.eclipsetrader.IChartData;
 import net.sourceforge.eclipsetrader.ui.internal.views.Messages;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 
 /**
  * Volume chart.
- * <p></p>
- * 
- * @author Marco Maccaferri
  */
 public class VolumeChart extends ChartPlotter
 {
   private static int VERTICAL_BORDER = 3;
+  private Color gridColor = new Color(null, 192, 192, 192);
+  private Color textColor = new Color(null, 0, 0, 0);
+  private NumberFormat nf = NumberFormat.getInstance();
+  
+  public VolumeChart()
+  {
+    nf.setGroupingUsed(false);
+    nf.setMinimumIntegerDigits(1);
+    nf.setMinimumFractionDigits(0);
+    nf.setMaximumFractionDigits(0);
+  }
 
   /* (non-Javadoc)
    * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#getDescription()
@@ -82,9 +93,80 @@ public class VolumeChart extends ChartPlotter
   }
 
   /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#paintGrid(org.eclipse.swt.graphics.GC, int, int)
+   */
+  public void paintGrid(GC gc, int width, int height)
+  {
+    int value = (int)getMax();
+    if (value >= 10000000)
+      value = value / 1000000 * 1000000;
+    else if (value >= 1000000)
+      value = value / 100000 * 100000;
+    else if (value >= 100000)
+      value = value / 1000 * 1000;
+    else
+      value = value / 100 * 100;
+
+    double pixelRatio = (height - VERTICAL_BORDER * 2) / (getMax() - getMin());
+    gc.setForeground(gridColor);
+    gc.setLineStyle(SWT.LINE_DOT);
+
+    int y1 = height - (int)((value - getMin()) * pixelRatio) - VERTICAL_BORDER;
+    gc.drawLine(0, y1, width, y1);
+    y1 = height - (int)(((value / 2) - getMin()) * pixelRatio) - VERTICAL_BORDER;
+    gc.drawLine(0, y1, width, y1);
+  }
+
+  /* (non-Javadoc)
    * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#paintScale(GC gc, int width, int height)
    */
   public void paintScale(GC gc, int width, int height)
   {
+    int value = (int)getMax();
+    if (value >= 10000000)
+      value = value / 1000000 * 1000000;
+    else if (value >= 1000000)
+      value = value / 100000 * 100000;
+    else if (value >= 100000)
+      value = value / 1000 * 1000;
+    else
+      value = value / 100 * 100;
+
+    double pixelRatio = (height - VERTICAL_BORDER * 2) / (getMax() - getMin());
+    gc.setForeground(textColor);
+    gc.setLineStyle(SWT.LINE_SOLID);
+
+    int y1 = height - (int)((value - getMin()) * pixelRatio) - VERTICAL_BORDER;
+    gc.drawLine(1, y1, 5, y1);
+    String s = nf.format(value);
+    if (getMax() >= 1000000)
+      s = nf.format(value / 1000);
+    gc.drawString(s, 10, y1 - gc.stringExtent(s).y / 2 - 1);
+    y1 = height - (int)(((value / 2) - getMin()) * pixelRatio) - VERTICAL_BORDER;
+    gc.drawLine(1, y1, 5, y1);
+    s = nf.format(value / 2);
+    if (getMax() >= 1000000)
+      s = nf.format(value / 1000 / 2);
+    gc.drawString(s, 10, y1 - gc.stringExtent(s).y / 2 - 1);
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#getValue(int, int)
+   */
+  public double getValue(int y, int height)
+  {
+    double pixelRatio = (height - VERTICAL_BORDER * 2) / (getMax() - getMin());
+    return (height - y) / pixelRatio + getMin();
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ui.views.charts.IChartPlotter#getFormattedValue(int, int)
+   */
+  public String getFormattedValue(int y, int height)
+  {
+    if (getMax() >= 1000000)
+      return nf.format(getValue(y, height) / 1000);
+    else
+      return nf.format(getValue(y, height));
   }
 }
