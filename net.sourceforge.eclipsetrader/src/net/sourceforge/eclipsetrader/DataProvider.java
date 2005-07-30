d0fc41f1de4d2d6ed81f31a8d6e57c94a663b4bb
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-07-30 09:33:04 +0000

    Changed for new observable collection methods and optimizations

--- net.sourceforge.eclipsetrader/src/net/sourceforge/eclipsetrader/DataProvider.java
+++ net.sourceforge.eclipsetrader/src/net/sourceforge/eclipsetrader/DataProvider.java
@@ -12,15 +12,16 @@ package net.sourceforge.eclipsetrader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Observable;
 
 import net.sourceforge.eclipsetrader.internal.StreamingControl;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
-import org.eclipse.core.runtime.Plugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * Default implementation of the IBasicDataProvider interface.
@@ -28,20 +29,18 @@ import org.osgi.framework.BundleContext;
  * override the startStreaming and stopStreaming methods only.</p>
  * <p></p>
  */
-public class DataProvider extends Plugin implements IBasicDataProvider, IExecutableExtension
+public class DataProvider implements IBasicDataProvider, IExecutableExtension, ICollectionObserver
 {
-  private static DataProvider instance;
   private List _listeners = new ArrayList();
   private HashMap _dataListeners = new HashMap();
   private boolean streaming = false;
-  protected IExtendedData[] data;
+  private IExtendedData[] data;
+  private Map symbolMap = new HashMap();
+  private Map tickerMap = new HashMap();
   
-  public static DataProvider getInstance() { return instance; }
-
   public DataProvider()
   {
-    super();
-    instance = this;
+    TraderPlugin.getDataStore().getStockwatchData().addObserver(this);
   }
   
   /* (non-Javadoc)
@@ -51,41 +50,18 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
   {
   }
 
-  /* (non-Javadoc)
-   * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-   */
-  public void start(BundleContext context) throws Exception
-  {
-    super.start(context);
-  }
-  
-  /* (non-Javadoc)
-   * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-   */
-  public void stop(BundleContext context) throws Exception
-  {
-    stopStreaming();
-    super.stop(context);
-  }
-
   /**
    * Add a data update listener.<br>
-   * The default implementation will call the startStreaming() method after the first
-   * listener is added.<br>
    * 
    * @param listener The listener to be removed.
    */
   public void addDataListener(IDataUpdateListener listener)
   {
     _listeners.add(listener);
-//    if (_listeners.size() == 1 && _dataListeners.size() == 0)
-//      startStreaming();
   }
 
   /**
    * Add a data update listener specific to a data item.<br>
-   * The default implementation will call the startStreaming() method after the first
-   * listener is added.<br>
    *
    * @param data The data item. 
    * @param listener The listener to be removed.
@@ -99,8 +75,6 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
       _dataListeners.put(data.getSymbol(), _v);
     }
     _v.add(listener);
-//    if (_listeners.size() == 0 && _dataListeners.size() == 1)
-//      startStreaming();
   }
 
   /**
@@ -112,6 +86,11 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
     {
       IDataUpdateListener l = (IDataUpdateListener)_listeners.get(i);
       l.dataUpdated(this);
+    }
+    for (int i = 0; i < getData().length; i++)
+    {
+      if (getData()[i] instanceof Observable)
+        ((Observable)getData()[i]).notifyObservers();
     }
   }
 
@@ -130,8 +109,8 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
         l.dataUpdated(this, data);
       }
     }
-    if (data instanceof ObservableObject)
-      ((ObservableObject)data).fireItemUpdated();
+    if (data instanceof Observable)
+      ((Observable)data).notifyObservers();
     
     // Notify generic listeners using the data-specific update notification method.
     // Avoids updating the same listener twice.
@@ -145,22 +124,16 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
   
   /**
    * Remove a data update listener specific to a data item.<br>
-   * The default implementation will call the stopStreaming() method after the last
-   * listener is removed.<br>
    * 
    * @param listener The listener to be removed.
    */
   public void removeDataListener(IDataUpdateListener listener)
   {
     _listeners.remove(listener);
-//    if (_listeners.size() == 0 && _dataListeners.size() == 0)
-//      stopStreaming();
   }
 
   /**
    * Remove a data update listener.<br>
-   * The default implementation will call the stopStreaming() method after the last
-   * listener is removed.<br>
    *
    * @param data The data item. 
    * @param listener The listener to be removed.
@@ -174,8 +147,6 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
       if (_v.size() == 0)
         _dataListeners.remove(data.getSymbol());
     }
-//    if (_listeners.size() == 0 && _dataListeners.size() == 0)
-//      stopStreaming();
   }
 
   /**
@@ -188,16 +159,7 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
     stopStreaming();
     _listeners.clear();
     _dataListeners.clear();
-  }
-
-  /**
-   * Set the data items for which the streaming is required.<br>
-   * 
-   * @param data The data array.
-   */
-  public void setData(IExtendedData[] data)
-  {
-    this.data = data;
+    TraderPlugin.getDataStore().getStockwatchData().removeObserver(this);
   }
 
   /**
@@ -207,7 +169,57 @@ public class DataProvider extends Plugin implements IBasicDataProvider, IExecuta
    */
   public IExtendedData[] getData()
   {
+    if (data == null)
+    {
+      List list = TraderPlugin.getDataStore().getStockwatchData();
+      data = new IExtendedData[list.size()];
+      list.toArray(data);
+    }
     return data;
+  }
+  
+  public IExtendedData getSymbol(String symbol)
+  {
+    if (symbolMap.isEmpty() == true)
+    {
+      for (Iterator iter = TraderPlugin.getDataStore().getStockwatchData().iterator(); iter.hasNext(); )
+      {
+        IExtendedData data = (IExtendedData)iter.next();
+        symbolMap.put(data.getSymbol(), data);
+      }
+    }
+    
+    return (IExtendedData)symbolMap.get(symbol);
+  }
+  
+  public IExtendedData getTicker(String ticker)
+  {
+    if (tickerMap.isEmpty() == true)
+    {
+      for (Iterator iter = TraderPlugin.getDataStore().getStockwatchData().iterator(); iter.hasNext(); )
+      {
+        IExtendedData data = (IExtendedData)iter.next();
+        tickerMap.put(data.getTicker(), data);
+      }
+    }
+    
+    return (IExtendedData)tickerMap.get(ticker);
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ICollectionObserver#itemAdded(java.lang.Object)
+   */
+  public void itemAdded(Object obj)
+  {
+    data = null;
+  }
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ICollectionObserver#itemRemoved(java.lang.Object)
+   */
+  public void itemRemoved(Object obj)
+  {
+    data = null;
   }
 
   /**
