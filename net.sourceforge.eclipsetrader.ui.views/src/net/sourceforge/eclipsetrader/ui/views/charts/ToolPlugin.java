b4409773ef4bd5acf6ec780bcd43e9827c80cfe9
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2005-08-11 14:14:51 +0000

    Added ChartObject extension

--- net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/ToolPlugin.java
+++ net.sourceforge.eclipsetrader.ui.views/src/net/sourceforge/eclipsetrader/ui/views/charts/ToolPlugin.java
@@ -10,57 +10,16 @@
  *******************************************************************************/
 package net.sourceforge.eclipsetrader.ui.views.charts;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Observable;
-import java.util.Observer;
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.Shell;
 
 
 /**
  */
-public abstract class ToolPlugin implements Observer
+public abstract class ToolPlugin extends ChartObject
 {
   private IAction action;
-  private Canvas canvas;
-  private Scaler scaler;
-  private boolean pressed = false;
-  protected HashMap parameters = new HashMap();
-
-  public Canvas getCanvas()
-  {
-    return this.canvas;
-  }
-  
-  public void setCanvas(Canvas canvas)
-  {
-    this.canvas = canvas;
-  }
-  
-  public Scaler getScaler()
-  {
-    return this.scaler;
-  }
-  
-  public void setScaler(Scaler scaler)
-  {
-    if (this.scaler != null)
-      this.scaler.deleteObserver(this);
-    this.scaler = scaler;
-    if (this.scaler != null)
-      this.scaler.addObserver(this);
-  }
-
-  public boolean containsPoint(int x, int y)
-  {
-    return false;
-  }
   
   public void setAction(IAction action)
   {
@@ -71,74 +30,19 @@ public abstract class ToolPlugin implements Observer
       action.setChecked(true);
     }
   }
-  
-  public void setParameter(String name, String value)
-  {
-    parameters.put(name, value);
-  }
-  
-  public String getParameter(String name)
-  {
-    return (String)parameters.get(name);
-  }
-  
-  public Map getParameters()
-  {
-    return parameters;
-  }
-  
-  /**
-   * Open the tool's parameters dialog.
-   * 
-   * @param parent - parent shell instance
-   * @return true if the user accepts the parameters, false otherwise
+
+  /* (non-Javadoc)
+   * @see net.sourceforge.eclipsetrader.ui.views.charts.ChartObject#mouseReleased(org.eclipse.swt.events.MouseEvent)
    */
-  public boolean openParametersDialog(Shell parent)
-  {
-    return false;
-  }
-
-  /**
-   * Return the state of the mouse button.
-   * 
-   * @return true if the mouse button is pressed, false otherwise
-   */
-  public boolean isMousePressed()
-  {
-    return pressed;
-  }
-
-  public void mousePressed(MouseEvent me)
-  {
-    pressed = true;
-  }
-
-  public void mouseDragged(MouseEvent me)
-  {
-  }
-
   public void mouseReleased(MouseEvent me)
   {
-    pressed = false;
+    super.mouseReleased(me);
     if (action != null)
     {
       action.setChecked(false);
       action.setEnabled(true);
       action = null;
     }
-  }
-  
-  /**
-   * Returns a rectangle describing the receiver's size and location.
-   * 
-   * @return the receiver's bounding rectangle
-   */
-  public Rectangle getBounds()
-  {
-    if (canvas != null)
-      return canvas.getBounds();
-    else
-      return new Rectangle(0, 0, 0, 0);
   }
 
   /**
@@ -148,18 +52,5 @@ public abstract class ToolPlugin implements Observer
    */
   public void paintTool(GC gc)
   {
-  }
-  
-  public void scalerUpdate()
-  {
-  }
-
-  /* (non-Javadoc)
-   * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
-   */
-  public void update(Observable o, Object arg)
-  {
-    if (o == scaler)
-      scalerUpdate();
   }
 }
