4c4236167cac73d7c4377b332c5c8c0d476068f9
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2006-04-15 09:12:49 +0000

    Added security wizard for Italian stocks

---
+++ net.sourceforge.eclipsetrader.directaworld/src/net/sourceforge/eclipsetrader/directaworld/wizards/SecurityWizard.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ */
+
+package net.sourceforge.eclipsetrader.directaworld.wizards;
+
+import java.util.Iterator;
+
+import net.sourceforge.eclipsetrader.core.CorePlugin;
+import net.sourceforge.eclipsetrader.core.db.Security;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ */
+public class SecurityWizard extends Wizard implements INewWizard
+{
+    public static String WINDOW_TITLE = "Italian Security Wizard";
+    private SecurityPage securityPage;
+
+    public SecurityWizard()
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+     */
+    public void init(IWorkbench workbench, IStructuredSelection selection)
+    {
+        setWindowTitle(WINDOW_TITLE);
+        
+        securityPage = new SecurityPage();
+        addPage(securityPage);
+    }
+
+    public void open()
+    {
+        setWindowTitle(WINDOW_TITLE);
+        WizardDialog dlg = create();
+        dlg.open();
+    }
+    
+    public WizardDialog create()
+    {
+        securityPage = new SecurityPage();
+        addPage(securityPage);
+
+        WizardDialog dlg = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), this);
+        dlg.create();
+        
+        return dlg;
+    }
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.Wizard#canFinish()
+     */
+    public boolean canFinish()
+    {
+        return securityPage.isPageComplete();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.Wizard#performFinish()
+     */
+    public boolean performFinish()
+    {
+        for (Iterator iter = securityPage.getSelectedSecurities().iterator(); iter.hasNext(); )
+            CorePlugin.getRepository().save((Security) iter.next());
+
+        return true;
+    }
+}
