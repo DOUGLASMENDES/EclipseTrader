1fbe3a49a728ce1ac0a2954f20c5afbaa2e095b6
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2006-05-01 07:26:41 +0000

    Added French news provider

---
+++ net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/FrenchNewsProvider.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ */
+
+package net.sourceforge.eclipsetrader.yahoo;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import net.sourceforge.eclipsetrader.core.CorePlugin;
+import net.sourceforge.eclipsetrader.core.INewsProvider;
+import net.sourceforge.eclipsetrader.core.db.NewsItem;
+import net.sourceforge.eclipsetrader.core.db.Security;
+import net.sourceforge.eclipsetrader.news.NewsPlugin;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.fetcher.FeedFetcher;
+import com.sun.syndication.fetcher.impl.FeedFetcherCache;
+import com.sun.syndication.fetcher.impl.HashMapFeedInfoCache;
+import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
+
+public class FrenchNewsProvider implements Runnable, INewsProvider
+{
+	private final static String NEWS_NAME =  "Yahoo! News (France)";
+	private final static String NEWS_CONFIG_FILE =  "categories.fr.xml";
+	private final static String NEWS_SECURITY_URL_BASE =  "http://fr.search.news.yahoo.com/search/compnews_fr?p=ytic:";
+	
+	private Thread thread;
+    private boolean stopping = false;
+    private FeedFetcherCache feedInfoCache = HashMapFeedInfoCache.getInstance();
+    private FeedFetcher fetcher = new HttpURLFeedFetcher(feedInfoCache);
+
+    public FrenchNewsProvider()
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.eclipsetrader.news.INewsProvider#start()
+     */
+    public void start()
+    {
+        if (thread == null)
+        {
+            stopping = false;
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.eclipsetrader.news.INewsProvider#stop()
+     */
+    public void stop()
+    {
+        stopping = true;
+        if (thread != null)
+        {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            thread = null;
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.eclipsetrader.news.INewsProvider#snapshot()
+     */
+    public void snapshot()
+    {
+        update();
+    }
+
+    /* (non-Javadoc)
+     * @see net.sourceforge.eclipsetrader.news.INewsProvider#snapshot(net.sourceforge.eclipsetrader.core.db.Security)
+     */
+    public void snapshot(Security security)
+    {
+        try {
+            update(new URL(NEWS_SECURITY_URL_BASE + security.getCode().toLowerCase()), security);
+        } catch(Exception e) {
+            CorePlugin.logException(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    public void run()
+    {
+        long nextRun = System.currentTimeMillis() + 2 * 1000;
+
+        while(!stopping)
+        {
+            if (System.currentTimeMillis() >= nextRun)
+            {
+                update();
+                int interval = NewsPlugin.getDefault().getPreferenceStore().getInt(NewsPlugin.PREFS_UPDATE_INTERVAL);
+                nextRun = System.currentTimeMillis() + interval * 60 * 1000;
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+        
+        thread = null;
+    }
+
+    private void update()
+    {
+        Job job = new Job(NEWS_NAME) {
+            protected IStatus run(IProgressMonitor monitor)
+            {
+                IPreferenceStore store = YahooPlugin.getDefault().getPreferenceStore();
+
+                List urls = new ArrayList();
+                try
+                {
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document document = builder.parse(YahooPlugin.getDefault().openStream(new Path(NEWS_CONFIG_FILE))); //$NON-NLS-1$
+
+                    NodeList childNodes = document.getFirstChild().getChildNodes();
+                    for (int i = 0; i < childNodes.getLength(); i++)
+                    {
+                        Node node = childNodes.item(i);
+                        String nodeName = node.getNodeName();
+                        if (nodeName.equalsIgnoreCase("category")) //$NON-NLS-1$
+                        {
+                            String id = ((Node)node).getAttributes().getNamedItem("id").getTextContent(); //$NON-NLS-1$
+                         
+                            NodeList list = node.getChildNodes();
+                            for (int x = 0; x < list.getLength(); x++)
+                            {
+                                Node item = list.item(x);
+                                nodeName = item.getNodeName();
+                                Node value = item.getFirstChild();
+                                if (value != null)
+                                {
+                                    if (nodeName.equalsIgnoreCase("url")) //$NON-NLS-1$
+                                    {
+                                        if (store.getBoolean(id))
+                                            urls.add(value.getNodeValue());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+//                List securities = CorePlugin.getRepository().allSecurities();
+//                monitor.beginTask("Fetching" + NEWS_NAME, securities.size() + urls.size());
+//
+//                for (Iterator iter = securities.iterator(); iter.hasNext(); )
+//                {
+//                    Security security = (Security) iter.next();
+//                    try {
+//                        String url = NEWS_SECURITY_URL_BASE + security.getCode().toLowerCase();
+//                        monitor.subTask(url);
+//                        update(new URL(url), security);
+//                    } catch(Exception e) {
+//                        CorePlugin.logException(e);
+//                    }
+//                    monitor.worked(1);
+//                }
+                for (Iterator iter = urls.iterator(); iter.hasNext(); )
+                {
+                    String url = (String) iter.next();
+                    try {
+                        monitor.subTask(url);
+                        update(new URL(url));
+                    } catch(Exception e) {
+                        CorePlugin.logException(e);
+                    }
+                    monitor.worked(1);
+                }
+
+                monitor.done();
+                return Status.OK_STATUS;
+            }
+        };
+        job.setUser(false);
+        job.schedule();
+    }
+
+    private void update(URL feedUrl)
+    {
+        update(feedUrl, null);
+    }
+
+    private void update(URL feedUrl, Security security)
+    {
+    	System.out.println("Fetching "+feedUrl.toExternalForm());
+    	
+    	boolean subscribersOnly = YahooPlugin.getDefault().getPreferenceStore().getBoolean(YahooPlugin.PREFS_SHOW_SUBSCRIBERS_ONLY);
+        
+        try {
+            SyndFeed feed = fetcher.retrieveFeed(feedUrl);
+            for (Iterator iter = feed.getEntries().iterator(); iter.hasNext(); )
+            {
+                SyndEntry entry = (SyndEntry) iter.next();
+                
+                if (!subscribersOnly && entry.getTitle().indexOf("[$$]") != -1)
+                    continue;
+                
+                NewsItem news = new NewsItem();
+                Date entryDate = entry.getPublishedDate();
+                if (entry.getUpdatedDate() != null)
+                    entryDate = entry.getUpdatedDate();
+                if (entryDate != null)
+                {
+                    Calendar date = Calendar.getInstance();
+                    date.setTime(entryDate);
+                    date.set(Calendar.SECOND, 0);
+                    news.setDate(date.getTime());
+                }
+                String title = entry.getTitle();
+                if (title.endsWith(")"))
+                {
+                    int s = title.lastIndexOf('(');
+                    if (s != -1)
+                    {
+                        news.setSource(title.substring(s + 1, title.length() - 1));
+                        title = title.substring(0, s - 1).trim();
+                    }
+                }
+                news.setTitle(title);
+                news.setUrl(entry.getLink());
+                if (security != null)
+                    news.setSecurity(security);
+//				System.out.println("** News found:");
+//				System.out.println(news.getTitle());
+//				System.out.println(news.getSource());
+//				System.out.println(news.getUrl());
+//				System.out.println("--");
+                CorePlugin.getRepository().save(news);
+            }
+        } catch(Exception e) {
+            CorePlugin.logException(e);
+        }
+    }
+}
---
+++ net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/preferences/FrenchNewsPreferencesPage.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ */
+
+package net.sourceforge.eclipsetrader.yahoo.preferences;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import net.sourceforge.eclipsetrader.yahoo.YahooPlugin;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class FrenchNewsPreferencesPage extends PreferencePage implements IWorkbenchPreferencePage
+{
+    private Button showSubscribersOnly;
+    private Table table;
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+     */
+    public void init(IWorkbench workbench)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
+     */
+    protected Control createContents(Composite parent)
+    {
+        Composite content = new Composite(parent, SWT.NONE);
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.numColumns = 2;
+        gridLayout.marginWidth = gridLayout.marginHeight = 0;
+        content.setLayout(gridLayout);
+        
+        showSubscribersOnly = new Button(content, SWT.CHECK);
+        showSubscribersOnly.setText("Get subscribers-only news");
+        showSubscribersOnly.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, false, false, 2, 1));
+        
+        table = new Table(content, SWT.FULL_SELECTION|SWT.SINGLE|SWT.CHECK);
+        table.setHeaderVisible(true);
+        GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+        gridData.heightHint = 250;
+        table.setLayoutData(gridData);
+        TableColumn column = new TableColumn(table, SWT.NONE);
+        column.setText("Provider");
+
+        IPreferenceStore store = YahooPlugin.getDefault().getPreferenceStore();
+        showSubscribersOnly.setSelection(store.getBoolean(YahooPlugin.PREFS_SHOW_SUBSCRIBERS_ONLY));
+
+        try
+        {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(YahooPlugin.getDefault().openStream(new Path("categories.fr.xml")));
+
+            NodeList childNodes = document.getFirstChild().getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++)
+            {
+                Node node = childNodes.item(i);
+                String nodeName = node.getNodeName();
+                if (nodeName.equalsIgnoreCase("category")) //$NON-NLS-1$
+                {
+                    String id = ((Node)node).getAttributes().getNamedItem("id").getTextContent();
+                    TableItem tableItem = new TableItem(table, SWT.NONE);
+                    NodeList list = node.getChildNodes();
+                    for (int x = 0; x < list.getLength(); x++)
+                    {
+                        Node item = list.item(x);
+                        nodeName = item.getNodeName();
+                        Node value = item.getFirstChild();
+                        if (value != null)
+                        {
+                            if (nodeName.equalsIgnoreCase("title"))
+                                tableItem.setText(value.getNodeValue());
+                        }
+                    }
+                    tableItem.setData(id);
+                    tableItem.setChecked(store.getBoolean(id));
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        table.getColumn(0).pack();
+
+        return content;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.preference.PreferencePage#performOk()
+     */
+    public boolean performOk()
+    {
+        IPreferenceStore store = YahooPlugin.getDefault().getPreferenceStore();
+
+        store.setValue(YahooPlugin.PREFS_SHOW_SUBSCRIBERS_ONLY, showSubscribersOnly.getSelection());
+        
+        TableItem[] items = table.getItems();
+        for (int i = 0; i < items.length; i++)
+        {
+            String id = (String) items[i].getData();
+            store.setValue(id, items[i].getChecked());
+        }
+
+        return super.performOk();
+    }
+}
