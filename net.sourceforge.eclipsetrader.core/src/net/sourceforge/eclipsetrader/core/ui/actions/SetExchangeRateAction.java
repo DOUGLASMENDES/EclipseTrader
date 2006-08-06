e2ab2581f29a040d7580167f50f0ec91d2335a23
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2006-08-06 21:53:31 +0000

    Added exchange rate dialog

---
+++ net.sourceforge.eclipsetrader.core/src/net/sourceforge/eclipsetrader/core/ui/actions/SetExchangeRateAction.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ */
+
+package net.sourceforge.eclipsetrader.core.ui.actions;
+
+import net.sourceforge.eclipsetrader.core.ui.dialogs.ExchangeRateDialog;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class SetExchangeRateAction implements IWorkbenchWindowActionDelegate, IViewActionDelegate
+{
+    private IWorkbenchWindow window;
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+     */
+    public void init(IWorkbenchWindow window)
+    {
+        this.window = window;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IViewActionDelegate#init(org.eclipse.ui.IViewPart)
+     */
+    public void init(IViewPart view)
+    {
+        this.window = view.getViewSite().getWorkbenchWindow();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+     */
+    public void dispose()
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(final IAction action)
+    {
+        ExchangeRateDialog dlg = new ExchangeRateDialog(window.getShell());
+        dlg.open();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection)
+    {
+    }
+}
