b9a22fbd1cb7ea24656b4d4c9abb8a07fbbb0594
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2007-01-25 08:06:57 +0000

    Added recent flag handling

--- net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/FrenchNewsProvider.java
+++ net.sourceforge.eclipsetrader.yahoo/src/net/sourceforge/eclipsetrader/yahoo/FrenchNewsProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * Copyright (c) 2004-2007 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -237,6 +237,7 @@ public class FrenchNewsProvider implements Runnable, INewsProvider
                     continue;
                 
                 NewsItem news = new NewsItem();
+                news.setRecent(true);
                 Date entryDate = entry.getPublishedDate();
                 if (entry.getUpdatedDate() != null)
                     entryDate = entry.getUpdatedDate();
