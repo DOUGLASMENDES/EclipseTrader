3c051d511c6e51a0fb24b050ed29f90650aefa55
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2007-08-02 06:46:44 +0000

    [core] Application startup code migrated to Eclipse 3.3

--- net.sourceforge.eclipsetrader/src/net/sourceforge/eclipsetrader/Application.java
+++ net.sourceforge.eclipsetrader/src/net/sourceforge/eclipsetrader/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2006 Marco Maccaferri and others.
+ * Copyright (c) 2004-2007 Marco Maccaferri and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,36 +8,49 @@
  * Contributors:
  *     Marco Maccaferri - initial API and implementation
  */
+
 package net.sourceforge.eclipsetrader;
 
-import org.eclipse.core.runtime.IPlatformRunnable;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * This class controls all aspects of the application's execution
  */
-public class Application implements IPlatformRunnable
-{
+public class Application implements IApplication {
 
-    /* (non-Javadoc)
-     * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
-     */
-    public Object run(Object args) throws Exception
-    {
-        Display display = PlatformUI.createDisplay();
-        try
-        {
-            int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-            if (returnCode == PlatformUI.RETURN_RESTART)
-            {
-                return IPlatformRunnable.EXIT_RESTART;
-            }
-            return IPlatformRunnable.EXIT_OK;
-        }
-        finally
-        {
-            display.dispose();
-        }
-    }
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	public Object start(IApplicationContext context) throws Exception {
+		Display display = PlatformUI.createDisplay();
+		try {
+			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
+			if (returnCode == PlatformUI.RETURN_RESTART)
+				return IApplication.EXIT_RESTART;
+			else
+				return IApplication.EXIT_OK;
+		} finally {
+			display.dispose();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	public void stop() {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		if (workbench == null)
+			return;
+		final Display display = workbench.getDisplay();
+		display.syncExec(new Runnable() {
+			public void run() {
+				if (!display.isDisposed())
+					workbench.close();
+			}
+		});
+	}
 }
