1cf5ae447b47c3791a55fce214827fc9207cbcce
maccaf <maccaf@a8e5a36e-6fb8-47cc-9500-2d10918a294f>
2008-06-28 21:44:11 +0000

    Added missing interfaces from level II implementation

---
+++ org.eclipsetrader.core/src/org/eclipsetrader/core/feed/ILevel2FeedSubscription.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2004-2008 Marco Maccaferri and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Maccaferri - initial API and implementation
+ */
+
+package org.eclipsetrader.core.feed;
+
+/**
+ * Manages the subscription of a feed identifier to a connector.
+ *
+ * @since 1.0
+ */
+public interface ILevel2FeedSubscription {
+
+	/**
+	 * Returns the identifier associated with this subscription.
+	 *
+	 * @return the identifier.
+	 */
+	public IFeedIdentifier getIdentifier();
+
+	/**
+	 * Disposes the receiver and all associated resources.
+	 */
+	public void dispose();
+
+	/**
+	 * Returns the current level II book.
+	 *
+	 * @return the level II book.
+	 */
+	public IBook getBook();
+
+	/**
+	 * Adds a listener to the collection of listeners that receive
+	 * notifications when a quote is updated.
+	 *
+	 * @param listener the listener to add.
+	 */
+	public void addSubscriptionListener(ISubscriptionListener listener);
+
+	/**
+	 * Removes a listener from the collection of listeners that receive
+	 * notifications when a quote is updated.
+	 *
+	 * @param listener the listener to remove.
+	 */
+	public void removeSubscriptionListener(ISubscriptionListener listener);
+}
